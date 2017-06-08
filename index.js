ddaa03769dc2937723f5c69ad6ee92e56eec6e47
liyun <eamil@example.com>
2017-06-08 21:26:26 +0800

    123

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/11/11.
+ */
+$(function () {
+  $('.one').on('click',function(){
+    $.ajax()
+  })
+});
---
+++ index.php
@@ -0,0 +1,11 @@
+[
+{user:"liyun";id:1;sex:"女";},
+{user:"liyun2";id:2;sex:"女";},
+{user:"liyun3";id:3;sex:"女";},
+{user:"liyun4";id:4;sex:"女";},
+{user:"liyun4";id:4;sex:"女";},
+{user:"liyun4";id:4;sex:"女";},
+{user:"liyun4";id:4;sex:"女";},
+{user:"liyun4";id:4;sex:"女";},
+
+]
